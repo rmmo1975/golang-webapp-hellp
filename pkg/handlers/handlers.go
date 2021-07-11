ffae021843373e989a36e4250e32c5ae6447e1ac
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-07-11 13:24:49 -0300

    added data template structure and example;

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,15 +4,19 @@ import (
 	"net/http"
 
 	"example.com/pkg/config"
+	"example.com/pkg/models"
 	"example.com/pkg/render"
 )
 
+// used by tthe handlers
 var Repo *Repository
 
+// repository type
 type Repository struct {
 	App *config.AppConfig
 }
 
+// creates a new repository
 func CreateRepo(a *config.AppConfig) *Repository {
 	return &Repository{
 		App: a,
@@ -25,9 +29,15 @@ func StartHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home-page.html")
+	render.RenderTemplate(w, "home-page.html", &models.TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about-page.html")
+	// add some logic here....
+	stringMap := make(map[string]string)
+	stringMap["test"] = "bolsonaro gosta de cheirar virilha suja"
+
+	render.RenderTemplate(w, "about-page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
