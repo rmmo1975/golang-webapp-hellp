b1d37c3204595595309a1ede52e2320d4e10edd6
Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
2021-07-11 23:02:01 -0300

    Merged in branch feature/03-add-routing-with-chi
    commit 253eb5fb8ae324490e8342b3d2ba3667924a2a50
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Sun Jul 11 22:58:52 2021 -0300
    
        added routes;
    
    commit 26c8ac4eba3461383ae75ae0dda0006c2641da39
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Sun Jul 11 22:58:23 2021 -0300
    
        as we have more files to execute, removed reference of main.go;
    
    commit 0e686c50b331b0c87f6e90ecae0b2eeca3f252ea
    Author: Rogerio Oliveira <rogerio.oliveira@whiteprompt.com>
    Date:   Sun Jul 11 22:57:29 2021 -0300
    
        tidy some dependencies;
    
    Changes to be committed:
    	modified:   .vscode/launch.json
    	modified:   cmd/web/main.go
    	new file:   cmd/web/routers.go
    	modified:   go.mod
    	modified:   go.sum

---
+++ cmd/web/routers.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"example.com/pkg/config"
+	"example.com/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
